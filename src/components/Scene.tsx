d42190980b126d5428940e2e17d42db8803ae563
Ben Halverson <benhalverson33@gmail.com>
2024-08-06 20:56:59 -0700

    refactor

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import STLModel from './STLModel';
-// import STL2Viewer from './STL2';
+import React, {Suspense, lazy} from "react";
+import PreviewComponent from "./PreviewComponent";
+
 
 const Scene: React.FC = () => {
-  return (
-    <Canvas>
-      <ambientLight/>
-      <pointLight position={[0, 0, 0]} />
-      {/* <STLModel url="https://pub-0ec69c7d5c064de8b57f5d594f07bc02.r2.dev/blender_rims.stl" /> */}
-      {/* <STL2Viewer url="https://pub-0ec69c7d5c064de8b57f5d594f07bc02.r2.dev/Front_rim_2wd_x2.stl" /> */}
-      <STLModel url="https://pub-0ec69c7d5c064de8b57f5d594f07bc02.r2.dev/blender_rims.stl" />
-      {/* <STLModel url="https://pub-0ec69c7d5c064de8b57f5d594f07bc02.r2.dev/Front_rim_2wd_x2.stl" /> */}
-      {/* <STLModel url="https://pub-0ec69c7d5c064de8b57f5d594f07bc02.r2.dev/Front_rim_2wd_x2.stl" /> */}
-      {/* <STLModel url="https://pub-0ec69c7d5c064de8b57f5d594f07bc02.r2.dev/Front%20rim.STL" /> */}
-      <gridHelper args={[250, 250]} position={[0, 0, 0]} />
-      <axesHelper args={[50]} />
+	return (
+		<div>
+			<PreviewComponent
+				url="https://pub-0ec69c7d5c064de8b57f5d594f07bc02.r2.dev/blender_rims.stl"
+				onExceedsLimit={() => false}
+				onError={() => (<div>
+          <p>There was an error loading the model</p>
+        </div>)}
+			/>
 
-      <OrbitControls />
-    </Canvas>
-  );
+		</div>
+	);
 };
 
 export default Scene;
